a61e5340a2b4c58523254e5c28313f9c7d1f0266
martendebruijn <hallo@martendebruijn.nl>
2021-10-02 22:23:22 +0200

    ✨feat (CountryFlag): new component

---
+++ src/components/CountryFlag.vue
@@ -0,0 +1,21 @@
+<template>
+  <span class="country-flag" role="img" :aria-label="name" :title="name">{{
+    flag
+  }}</span>
+</template>
+
+<script>
+export default {
+  name: 'CountryFlag',
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    flag: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
