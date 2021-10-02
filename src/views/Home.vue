f3a8033a1a96511645b5c327e1113c6eb8e35d42
martendebruijn <hallo@martendebruijn.nl>
2021-10-02 23:27:12 +0200

    ✨feat (Home): added home view

---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <!-- <SingletonHeader /> -->
+  <h1 class="text-5xl font-bold sm:text-7xl md:text-8xl lg:text-9xl"
+    >Where have I been?</h1
+  >
+  <!-- <TravelStats title="Visited:" /> -->
+  <CountryForm />
+</template>
+
+<script>
+// import { v4 as uuidv4 } from 'uuid'
+// import countries from '@/assets/countries.json'
+// import SingletonHeader from '@/components/singletons/SingletonHeader.vue'
+// import TravelStats from '@/components/TravelStats.vue'
+import CountryForm from '@/components/CountryForm.vue'
+export default {
+  name: 'Home',
+  components: {
+    // SingletonHeader,
+    // TravelStats,
+    CountryForm,
+  },
+}
+</script>
