fd428e938d8591586b2fa934fa0ca93f9674c46d
martendebruijn <hallo@martendebruijn.nl>
2021-10-02 23:26:32 +0200

    🔨fix (main): added vue-router

--- src/main.js
+++ src/main.js
@@ -10,10 +10,11 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { fas } from '@fortawesome/free-solid-svg-icons'
+import router from './router'
 
 // Global components
 const FAStyles = [fab, fas]
 FAStyles.forEach((style) => library.add(style))
 
 const app = createApp(App)
-app.component('FontAwesomeIcon', FontAwesomeIcon).mount('#app')
+app.use(router).component('FontAwesomeIcon', FontAwesomeIcon).mount('#app')
