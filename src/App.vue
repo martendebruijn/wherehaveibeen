0653504851db5b74d5a5a6d890d8bb80ead7ba45
martendebruijn <hallo@martendebruijn.nl>
2021-10-02 23:24:13 +0200

    🔨fix (App): moved logic from app to seperate pages

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,3 @@
 <template>
-  <!-- <SingletonHeader /> -->
-  <h1 class="text-5xl font-bold sm:text-7xl md:text-8xl lg:text-9xl"
-    >Where have I been?</h1
-  >
-  <!-- <TravelStats title="Visited:" /> -->
-  <CountryForm />
+  <router-view />
 </template>
-
-<script>
-// import { v4 as uuidv4 } from 'uuid'
-// import countries from '@/assets/countries.json'
-// import SingletonHeader from '@/components/singletons/SingletonHeader.vue'
-// import TravelStats from '@/components/TravelStats.vue'
-import CountryForm from '@/components/CountryForm.vue'
-
-export default {
-  name: 'App',
-  components: {
-    // SingletonHeader,
-    // TravelStats,
-    CountryForm,
-  },
-}
-</script>
