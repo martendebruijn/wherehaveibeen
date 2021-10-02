70106e59dbf89f9cda54b47a5b8a38e5303fc148
martendebruijn <hallo@martendebruijn.nl>
2021-10-02 22:22:03 +0200

    ✨feat (SingletonHeader): nieuw component

---
+++ src/components/singletons/SingletonHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="singleton-header bg-blue-800 flex">
+    <span>Some header</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SingletonHeader',
+}
+</script>
+
+<style lang="scss" scoped></style>
