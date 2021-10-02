66c70cdc95f127584c0849310c89c4abbb35fbbd
martendebruijn <hallo@martendebruijn.nl>
2021-10-02 20:07:11 +0200

    ✨feat added fontawesome

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
+// Vue Imports
 import { createApp } from 'vue'
 import App from './App.vue'
+
+// CSS Imports
 import './assets/styles/index.css'
 
-createApp(App).mount('#app')
+// FontAwesome Imports
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+
+// Global components
+const FAStyles = [fab, fas]
+FAStyles.forEach((style) => library.add(style))
+
+const app = createApp(App)
+app.component('FontAwesomeIcon', FontAwesomeIcon).mount('#app')
